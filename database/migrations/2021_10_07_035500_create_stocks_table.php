6aa074f320cb36eedd77e7a85843a9bf51e2d013
Muntasirornob <muntasirornob.com>
2021-10-11 14:27:11 +0600

    adding mesbas filels

--- database/migrations/2021_10_07_035500_create_stocks_table.php
+++ database/migrations/2021_10_07_035500_create_stocks_table.php
@@ -17,10 +17,10 @@ class CreateStocksTable extends Migration
             $table->id();
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('supplier_id');
+            $table->unsignedBigInteger('purchases_id');
             $table->date('product_add_date');
             $table->integer('product_stock_count');
-            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('supplier_id')->references('id')->on('suppliers')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('purchases_id')->references('id')->on('purchases')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
