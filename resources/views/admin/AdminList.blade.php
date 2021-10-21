aca3123a20f171708a631b511520c4c08f988387
Mesbaul-alom <mesbaalam90@gmail.com>
2021-10-21 11:15:57 +0600

    adminmanager update

--- resources/views/admin/AdminList.blade.php
+++ resources/views/admin/AdminList.blade.php
@@ -11,13 +11,13 @@
         <!-- Start Content-->
         <div class="container-fluid">
 
-            <div class="row">
+            <div class="row" style="padding-top: 50px">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
 
                             <h4 class="header-title">Admin List</h4>
-                           
+
                             <table id="datatable-buttons" class="table table-striped dt-responsive nowrap w-100">
                                 <thead>
                                     <tr>
@@ -28,24 +28,24 @@
                                         <th>Action</th>
                                     </tr>
                                 </thead>
-                            
-                            
+
+
                                 <tbody>
                                     @foreach($admins as $admin)
                                     <tr>
-                                        <td> 
+                                        <td>
                                             <div class="avatar-sm mx-auto mb-4">
                                                 <span class="avatar-title rounded-circle bg-soft-primary text-primary font-size-16">
                                                     <img src="{{('/admin_img/'.$admin->image)}}" alt="" class="img-thumbnail rounded-circle">
                                                 </span>
                                             </div>
 
-                                
+
                                         </td>
                                         <td>{{$admin->username}}</td>
                                         <td>{{$admin->fullname}}</td>
                                         <td>{{$admin->email}}</td>
-                                       
+
                                         <td>
                                     <a href="/edit/admin/{{$admin->id}}" class="btn btn-primary">Edit</a>
                                     <a href="/delete/admin/{{$admin->id}}" class="btn btn-danger" id="delete">Delete</a>
@@ -54,7 +54,7 @@
                                     @endforeach
                                 </tbody>
                             </table>
-                            
+
                         </div> <!-- end card body-->
                     </div> <!-- end card -->
                 </div><!-- end col-->
@@ -63,6 +63,6 @@
     </div>
  </div>
  <script>
-     
+
  </script>
 @endsection
